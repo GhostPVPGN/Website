73e04db8ceb783afec0efe15a05ce7223962cc9e
GhostPVPGN <60575346+GhostPVPGN@users.noreply.github.com>
2020-02-07 13:06:00 +0000

    Update webconf.php

--- config/webconf.php
+++ config/webconf.php
@@ -1,15 +1,15 @@
 <?php
-$servertitle = "YOuR NAME";
-$realmlist = "YOuR IP";
-$startladder = "<br><font color = #33A9FF> YOuR DATE </font>";
-$resetladder = "<br><font color = #33A9FF> YOuR DATE </font>";
-$launchdate = "<br><font color = #33A9FF> YOuR DATE </font>";
-$gameversion = "<br><font color = #33A9FF> YOuR VERSION</font>";
-$xprateone = "<font color = #33A9FF> YOuR RATE </font>";
-$xpratetwo = "<font color = #33A9FF> YOuR RATE </font>";
-$xpratethree = "<font color = #33A9FF> YOuR RATE </font>";
-$xpratefour = "<font color = #33A9FF> YOuR RATE </font>";
-$clientdownload = " YOuR LINK ";
-$mpqdownload = "YOuR LINK";
-$toolsdownload = "YOuR LINK";
+$servertitle = "YOUR NAME";
+$realmlist = "YOUR IP";
+$startladder = "<br><font color = #33A9FF> YOUR DATE </font>";
+$resetladder = "<br><font color = #33A9FF> YOUR DATE </font>";
+$launchdate = "<br><font color = #33A9FF> YOUR DATE </font>";
+$gameversion = "<br><font color = #33A9FF> YOUR VERSION</font>";
+$xprateone = "<font color = #33A9FF> YOUR RATE </font>";
+$xpratetwo = "<font color = #33A9FF> YOUR RATE </font>";
+$xpratethree = "<font color = #33A9FF> YOUR RATE </font>";
+$xpratefour = "<font color = #33A9FF> YOUR RATE </font>";
+$clientdownload = " YOUR LINK ";
+$mpqdownload = "YOUR LINK";
+$toolsdownload = "YOUR LINK";
 ?>
